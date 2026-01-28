50bbbb91cf57116578747927df9cce95a550a97d
willowhughes <willow.n.hughes@gmail.com>
2026-01-27 20:28:05 -0800

    messin around

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import './style.css'
 const app = document.getElementById('app');
 
 if (app) {
-  app.innerHTML = `
-    <h1>Hello TypeScript</h1>
-    <p>Start building here</p>
-  `;
+  // app.innerHTML = `
+  //   <h1>Hello TypeScript</h1>
+  //   <p>Start building here</p>
+  // `;
 }
